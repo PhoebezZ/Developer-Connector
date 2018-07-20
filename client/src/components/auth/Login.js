b39cb234936ec683c5c949c3fc0344809180f308
Phoebe <phoebejiangli@gmail.com>
2018-07-19 20:36:04 -0700

    test

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,7 +4,7 @@ class Login extends Component {
     render() {
         return (
             <div>
-                <h1>This is Login</h1>
+                Login
             </div>
         )
     }
