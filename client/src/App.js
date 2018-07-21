60588c2b196d541387a3128d0b9c49eab7ebd47f
Phoebe <phoebejiangli@gmail.com>
2018-07-21 14:32:21 -0700

    Logout & navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import store from './store/configureStore';
 
 import Navbar from './components/layout/Navbar';
@@ -22,6 +22,17 @@ if (localStorage.jwtToken) {
     const decoded = jwt_decode(localStorage.jwtToken);
     // Set user and isAuthenticated
     store.dispatch(setCurrentUser(decoded));
+    // Check for expired token
+    const currentTime = Date.now() / 1000;
+    if (decoded.exp < currentTime) {
+        // Logout user
+        store.dispatch(logoutUser());
+        // Clear current Profile
+
+        // Redirect to login
+        // this.props.history.push('/login');
+        window.location.href = '/login';
+    }
 }
 
 class App extends Component {
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
 import './index.css';
 import icon from '../../img/icon.png';
 
-
 class Navbar extends Component {
+    onLogoutClick = e => {
+        e.preventDefault();
+        this.props.logoutUser();
+    }
+
     render() {
+        const { isAuthenticated, user } = this.props.auth;
+
+        const authLinks = (
+            <ul className="navbar-nav ml-auto">
+                <a href="" onClick={this.onLogoutClick} className="nav-link">
+                    <img
+                        className="rounded-circle"
+                        src={user.avatar}
+                        alt={user.name}
+                        style={{ width: '25px', marginRight: '5px' }}
+                        title="You must have a Gravatar connected to your email to display an image"
+                    /> Logout
+                </a>
+            </ul>
+        );
+
+        const guestLinks = (
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <Link className="nav-link" to="/register">Sign Up</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/login">Login</Link>
+                </li>
+            </ul>
+        );
+
         return (
             <nav className="navbar navbar-expand-sm mb-4 navbar-dark">
                 <div className="container">
@@ -20,15 +54,7 @@ class Navbar extends Component {
                                 <Link className="nav-link" to="/profiles"> Developers</Link>
                             </li>
                         </ul>
-
-                        <ul className="navbar-nav ml-auto">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/register">Sign Up</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/login">Login</Link>
-                            </li>
-                        </ul>
+                        {isAuthenticated ? authLinks : guestLinks}
                     </div>
                 </div>
             </nav>
@@ -36,4 +62,13 @@ class Navbar extends Component {
     }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => ({
+    auth: state.auth
+});
+
+Navbar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+};
+
+export default connect(mapStateToProps, { logoutUser })(Navbar);
