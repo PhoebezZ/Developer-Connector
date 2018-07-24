c5268f7a7e06ce80cb65606824dfde7764bf1096
Phoebe <phoebejiangli@gmail.com>
2018-07-24 13:47:10 -0700

    Profile about & credentials

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types';
 import isEmpty from '../../validation/is-empty';
 
 class ProfileAbout extends Component {
@@ -19,11 +18,11 @@ class ProfileAbout extends Component {
             <div className="row">
                 <div className="col-md-12">
                     <div className="card card-body bg-light mb-3">
-                        <h3 className="text-center text-info">{firstName}'s Bio</h3>
+                        <h3 className="text-center" style={{ color: '#62B8B1' }}>{firstName}'s Bio</h3>
                         <p className="lead">{isEmpty(profile.bio) ? (<span>{firstName} does not have a bio</span>) : (<span>{profile.bio}</span>)}
                         </p>
                         <hr />
-                        <h3 className="text-center text-info">Skill Set</h3>
+                        <h3 className="text-center" style={{ color: '#62B8B1' }}>Skill Set</h3>
                         <div className="row">
                             <div className="d-flex flex-wrap justify-content-center align-items-center">
                                 {skills}
