c8cb26a2d581b6d6350cdb520c49865ea5f71eb7
Phoebe <phoebejiangli@gmail.com>
2018-07-24 19:08:25 -0700

    profile github

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 import isEmpty from '../../validation/is-empty';
 
 class ProfileAbout extends Component {
@@ -33,6 +34,10 @@ class ProfileAbout extends Component {
             </div>
         )
     }
+}
+
+ProfileAbout.propTypes = {
+    profile: PropTypes.object.isRequired,
 }
 
 export default ProfileAbout;
--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,13 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class ProfileGithub extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            clientId: '6fa0c723bcbce2420106',
+            clientSecret: '5906aa71ce20376879b1e46de7151704f373f62a',
+            count: 5,
+            sort: 'created: asc',
+            repos: []
+        };
+    }
+
+    componentDidMount() {
+        const { username } = this.props;
+        const { count, sort, clientId, clientSecret } = this.state;
+
+        fetch(`https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`)
+            .then(res => res.json())
+            .then(data => {
+                if (this.refs.myRef) {
+                    this.setState({ repos: data });
+                }
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
+        const { repos } = this.state;
+
+        const repoItems = repos.map(repo => (
+            <div key={repo.id} className="card card-body mb-2">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h4>
+                            <Link to={repo.html_url} className="text" target="_blank" style={{ color: '#62B8B1' }}>
+                                {repo.name}
+                            </Link>
+                        </h4>
+                        <p>{repo.description}</p>
+                    </div>
+                    <div className="col-md-">
+                        <span className="badge mr-1" style={{ backgroundColor: '#62B8B1', color: 'white' }}>
+                            Stars: {repo.stargazers_count}
+                        </span>
+                        <span className="badge badge-secondary mr-1">
+                            Watchers: {repo.watchers_count}
+                        </span>
+                        <span className="badge badge-success">
+                            Forks: {repo.forks_count}
+                        </span>
+                    </div>
+                </div>
+            </div >
+        ))
+
         return (
-            <div>
-                <h1>Profile github</h1>
+            <div ref="myRef">
+                <hr />
+                <h3 className="mb-4">Latest Github Repos</h3>
+                {repoItems}
             </div>
         )
     }
+}
+
+ProfileGithub.propTypes = {
+    username: PropTypes.string.isRequired,
 }
 
 export default ProfileGithub;
