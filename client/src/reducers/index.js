eea538a18788a3b1d39c1f3f3818ae215b7812a6
Phoebe <phoebejiangli@gmail.com>
2018-07-24 20:13:07 -0700

    Post component

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import errorRefucer from './errorReducer';
 import profileReducer from './profileReducer';
+import postReducer from './postReducer';
 
 const reducers = combineReducers({
     auth: authReducer,
     errors: errorRefucer,
-    profile: profileReducer
+    profile: profileReducer,
+    post: postReducer
 });
 
 export default reducers;
